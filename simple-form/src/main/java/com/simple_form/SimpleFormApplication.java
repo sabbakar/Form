f59a6d9e053d53eaa09c23451f19c86d3f5ef06e
sabbakar <sadeeqsas14@gmail.com>
2024-07-18 21:58:41 +0100

    pushed 2

--- simple-form/src/main/java/com/simple_form/SimpleFormApplication.java
+++ simple-form/src/main/java/com/simple_form/SimpleFormApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.simple_form.model")
+@EnableJpaRepositories(basePackages = "com.simple_form.repository")
 @SpringBootApplication
 public class SimpleFormApplication {
 
